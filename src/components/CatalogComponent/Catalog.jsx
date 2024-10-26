ec59531db72de66d5212d3834706b25c45e657c6
DenisSleep <160733810+DenisSleep@users.noreply.github.com>
2024-10-27 00:14:57 +0300

    Реакт 6

---
+++ src/components/CatalogComponent/Catalog.jsx
@@ -0,0 +1,27 @@
+import style from './Catalog.module.css'
+import { Card } from '../CardComponent/Card'
+import { Category } from '../CategoryComponent/CategoryComponent'
+import { products } from '../../data/data'
+
+
+export function Catalog() {
+    return (
+        <>
+            <div className={style.catalog}>
+                <p className={style.catalog_title}>Каталог товаров</p>
+                <div className={style.categories}>
+                    <Category />
+                </div>
+            </div>
+            <div className={style.catalog_grid}>
+                {
+                    products.map((product) => {
+                        return (
+                            <Card {...product} />
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
